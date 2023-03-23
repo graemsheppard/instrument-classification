232ad944a5a1436454b5b03c93420b5ae1a2ebf2
LordSir101 <tywunderlich15@gmail.com>
2023-03-23 19:54:06 -0400

    use testing data to make predictions

--- src/main.py
+++ src/main.py
@@ -8,27 +8,61 @@ from scipy.io import wavfile
 from keras.models import load_model
 from keras.engine.sequential import Sequential
 
-from util import SAMPLE_SIZE, STEP_SIZE, transform
+from util import SAMPLE_SIZE, STEP_SIZE, LABELS, transform
 
 
 def main():
-
-    # All code below is just demoing the prediction on the first file
-
-    data_dir = "training_data"
-
-    test_file = os.path.join(data_dir, "cel", "[cel][cla]0001__1.wav")
-    test_sample_rate, test_audio = wavfile.read(test_file)
-
     model: Sequential = load_model('saved_model')
 
-    idx = 0
-    while idx + SAMPLE_SIZE < len(test_audio):
-        test_samples = test_audio[idx : idx + SAMPLE_SIZE, :]
-        test_freqs = transform(test_samples)
-        x_test = np.expand_dims(test_freqs, axis=0)
-        y_test = model.predict(x_test, verbose=0)
-        idx = idx + STEP_SIZE
+    data_dir = "testing_data"
+    files = os.listdir(data_dir)
+    
+    print(files)
+
+    for idx, file in enumerate(files):
+        extension = file.split('.')[1]
+        if extension == 'wav':
+            continue
+
+        print('--------------------------------------------------')
+
+        labels = []
+        with open(os.path.join(data_dir,file), "r") as label_file:
+            data = label_file.read()
+            lines = data.split('\n')
+            labels = [label for label in lines]
+        
+        audio_file = os.path.join(data_dir, f'{file.split(".")[0]}.wav')
+         
+        test_sample_rate, test_audio = wavfile.read(audio_file)
+
+        sums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        idx = 0
+        num_samples = 0
+        while idx + SAMPLE_SIZE < len(test_audio):
+            num_samples+=1
+            test_samples = test_audio[idx : idx + SAMPLE_SIZE, :]
+            test_freqs = transform(test_samples)
+            x_test = np.expand_dims(test_freqs, axis=0)
+            try:
+                y_test = model.predict(x_test, verbose=0)
+            except Exception as e:
+                print(e)
+                break
+
+            sums = [averages[i] + value for i, value in enumerate(y_test[0])]
+            idx = idx + STEP_SIZE
+        
+        averages = [sum/num_samples for sum in sums]
+        
+        print(f'Labels: {labels}')
+        print('Predicted:')
+        print(averages)
+
+        for i, average in enumerate(averages):
+            if average > 0.2:
+                print(LABELS[i])
 
 if __name__ == "__main__":
     main()
